373904746bd280ef38ccbff0c92e3edd3fce11db
Cal Br <calcbradley@gmail.com>
2025-06-13 22:14:45 +0100

    text highlighting test

--- kamehouse.js
+++ kamehouse.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 // Start the server
@@ -19,7 +19,22 @@ app.listen(PORT, (err) => {
 //
 // VIEWS
 //
-// Render kamislookout.ejs
+
+// The sentence and word to highlight
+const sentence = "I really like to eat a banana every morning.";
+const wordToHighlight = "every";
+
+// app.get('/', (req, res) => {
+//   const regex = new RegExp(`\\b${wordToHighlight}\\b`, 'gi');
+//   const highlightedSentence = sentence.replace(regex, `<span class="highlight">${wordToHighlight}</span>`);
+//   res.render('kamislookout', { sentence: highlightedSentence });
+// });
+
 app.get('/', (req, res) => {
-  res.render('kamislookout.ejs');
+  const regex = new RegExp(`\\b${wordToHighlight}\\b\\s+(\\w+)`, 'i'); // match word + space + capture next word
+  const highlightedSentence = sentence.replace(regex, (match, nextWord) => {
+    return match.replace(nextWord, `<span class="highlight">${nextWord}</span>`);
+  });
+
+  res.render('kamislookout', { sentence: highlightedSentence });
 });
